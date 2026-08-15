d2cfec7af2b0c13371bbdd018585ff4c00b3a0e9
Tomas Rimkevicius <tomas.ew88@gmail.com>
2026-08-15 13:16:24 +0300

    Allow users to access the sign  up endpoint while being unauthenticated

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,11 +1,13 @@
 class UsersController < ApplicationController
+  allow_unauthenticated_access only: :create
+
   def create
     user_params = params.permit(:email_address, :password)
     user = User.new(user_params)
     if user.save
       render status: :created
     else
-      render json: { errors: user.errors }, status: :unproccessable_entity
+      render json: { errors: user.errors }, status: :unprocessable_entity
     end
   end
 end
